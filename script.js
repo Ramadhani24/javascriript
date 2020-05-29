d665edfa11a683f4c504fdc19e85bf60eaf4512b
Muhammad Ramadhani <64588953+Ramadhani24@users.noreply.github.com>
2020-05-29 21:05:31 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-var x = 10;
-		console.log('Hello word');
-		console.log('isi dari variable x adalah ' + x);
+alert ('selamat datang..');
+var lagi = true;
+
+while( lagi === true);
+var nama = prompt ('masukan nama');
+alert ('halo'+ nama);
+
+lagi = confirm ('coba lagi');
+
+
+alert('terima kasih..');
