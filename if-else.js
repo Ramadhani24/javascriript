7cdf825a52d09267202c119510c9d767d9083b6a
Muhammad Ramadhani <64588953+Ramadhani24@users.noreply.github.com>
2020-06-01 14:41:35 +0700

    Add files via upload

---
+++ if-else.js
@@ -0,0 +1,8 @@
+var angkat = ('masukan angkat')
+if ( angkat % 2 == 0 ) {
+	alert(angkat + 'adalah bilangan genap');
+}else if ( angkat % 2 === 1) {
+	alert (angkat + 'adalah bilangan ganjil');
+} else {
+	alert('yang anda masukan bukan angkat!');
+}
