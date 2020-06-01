a4b2fc5c205b1ba4c6f9a190cab0904e644b063d
Muhammad Ramadhani <64588953+Ramadhani24@users.noreply.github.com>
2020-06-01 20:40:32 +0700

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,9 @@
+var angka = 1
+
+if( angka == 1) {
+	alert ('anda memasukan angka 1');
+} else if ( angka == 2) {
+	alert ('anda memasukan angka 2')
+} else {
+	alert (' anda yang anda memasukan salah')
+}
