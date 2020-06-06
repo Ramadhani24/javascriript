e1bf9b79b11c243b2ed0de7b57e6f396c7148390
Muhammad Ramadhani <64588953+Ramadhani24@users.noreply.github.com>
2020-06-06 13:54:32 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
-function tampilangka(n) {
-	if( n=== 0) return;
-	console.log
-	return tampilangka(n-1)
-}
+// manipulasi Array
 
-tampilangka(10)
+// 1. menambah isi array
+// var arr = {[];
+// arr [0] = "sandhika";
+// arr [1] = "galih"
+// arr [2] = "nofa"
+// arr [6] = "doddy"
+
+// console.log
+
+// 2.menghapus isi aray
+// var arr = ["sandhika, galih, nofa, doddy"];
+// arr[1] = undefined;
+// console.log(arr);
+
+
+// 3. menampilkan isi array
+
+// 2.menghapus isi aray
+ //var arr = ["sandhika, galih, nofa,"];
+
+//for(var i = 0; i < arr.lenght; i++) {
+//	console.log('mahasiswa ke-'+(i+1) +':' + arr(i);
+//}
+
+// method pada array
+// 1.join
+
+// 2.menghapus isi aray
+var arr = ["sandhika, galih, nofa, "];
+// 1. join
+//console.log(arr.join())
+// 2. push & pop
+//arr.push('doddy','fitri')
+// arr.pop();
+// arr.pop();
+console.log(arr.join(' - '));
+
+// 3.unsihift& shift
+// arr.unsihift('doddy')
+console.log(arr.join(' - '));
