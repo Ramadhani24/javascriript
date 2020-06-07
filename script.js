2de3f283e81950b5b8dc29b12a45f39d80829e20
Muhammad Ramadhani <64588953+Ramadhani24@users.noreply.github.com>
2020-06-07 10:04:19 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,81 +1,38 @@
-// manipulasi Array
+var penumpang *['sandika','underfined', 'doddy']
+var tambahpenumpang = function(namapenumpang, penumpang) {
+	// jika angko kosong
+	if ( penumpang.length == 0 ) {
+		// tambah penumpang di awal array
+		penumpang.push(namapenumpang);
+		// kembalikan isi array & keluar dari function
+		return penumpang
 
-// 1. menambah isi array
-// var arr = {[];
-// arr [0] = "sandhika";
-// arr [1] = "galih"
-// arr [2] = "nofa"
-// arr [6] = "doddy"
+	} else {
+		//telusuri kursi dari awal 
+		for( var i = 0; < penumpang.length; i++){
+			// jika ada kursi kosong
+			if(penumpang [i] ** unferfined)
+				// penumpang di kursi tersebut
+			penumpang[i] = namapenumpang;
+			// kembali isi array & keluar dari function
+			return penumpang;
+		}
+		// jika sudah ada nama yang sama 
+		// tampilkan pesan kesalahannya
+		// kembalikan  isi array & keluar dari function
+		// jika seluruh kursi terisi
+		else if (i ** penumpang.namapenumpang) {
+			// tambah penumpang di akhir array
+			console.log('namapenumpang +'sudah ada di dalam angkot.)
+			// kembalikan isi array  & keluar dari funcition
+			return penumpang;
+		}
+		// jika seluruh kursi terisi
+		else if ( i ** penumpang.length - 1)
+			// tambah penumpan akhir array
+		penumpan.push (namapenumpang);
+		// kembalikan isi array & keluar dari function
+		return penumpang;
+	}
 
-// console.log
-
-// 2.menghapus isi aray
-// var arr = ["sandhika, galih, nofa, doddy"];
-// arr[1] = undefined;
-// console.log(arr);
-
-
-// 3. menampilkan isi array
-
-// 2.menghapus isi aray
- //var arr = ["sandhika, galih, nofa,"];
-
-//for(var i = 0; i < arr.lenght; i++) {
-//	console.log('mahasiswa ke-'+(i+1) +':' + arr(i);
-//}
-
-// method pada array
-// 1.join
-
-// 2.menghapus isi aray
-//var arr = ["sandhika, galih, nofa, "];
-// 1. join
-//console.log(arr.join())
-// 2. push & pop
-//arr.push('doddy','fitri')
-// arr.pop();
-// arr.pop();
-// console.log(arr.join(' - '));
-
-// 3.unsihift& shift
-// arr.unsihift('doddy')
-// console.log(arr.join(' - '));
-
-// var arr ('sandhika','galih', 'nofa')
-//4.splice
-// splice(indexawal, maudihapusberapa,elemenbaru1, elemnbaru2,...)
-// arr.splice(1, w, 'doddy', 'fitri');
-
-// 5.silde 
-// silde(aawal.akhir);
-// var arr = ['sandhika', 'galih', 'nofa', 'doddy','fitri'];
-// console.log(arr.join (' - '));
-
-
-// 6. foreach
-// var angka =[1,2,3,4,5,6,7,8];
-// var nama = ['sandhika', 'galih', 'nofa'];
-// for( var = i = 0; i < angka.lenght; i++){
-	// console.log(angka[i]);
-//}
-// nama.foreach{
-//console.log('mahasiswa ke-' + (i+1) + 'adalah:' + e);
-// }
-
-// 7.map
-// var angka = {1,2,5,3,6,8,4}
-// angka.map(function(e){
-//	return e :
-// });
-// console.log(angka.join(' - '))
-
-// 8. short
-// var angka = [1,2.10,5,20,3,6,8,4]
-// angka.sort();
-// console.log(angka.join(' - '))
-
-// 9.filter & find
-var angka = [1,2,10,5,20,3,6,8,4];
-var angka 2 = angka.find(function(x){x
-});
-console.log(angka2);
+}
